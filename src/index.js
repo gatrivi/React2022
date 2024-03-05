04e1d9c62cc6af274960b754ef0097613cd7e160
Developer Formerly Known as Decat <62384085+gatrivi@users.noreply.github.com>
2024-03-05 18:39:34 -0300

    updated html tags

--- src/index.js
+++ src/index.js
@@ -1,21 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
-import { BrowserRouter } from 'react-router-dom'
-import { Provider } from 'react-redux'
-import { store, persistor } from './redux/store'
-import { PersistGate } from 'redux-persist/integration/react'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store, persistor } from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
-      //from here persistor will receive the store //and also rehydrate app each
-      time app refreshes
       <PersistGate persistor={persistor}>
         <App />
       </PersistGate>
     </BrowserRouter>
   </Provider>,
-  document.getElementById('root')
-)
+  document.getElementById("root")
+);
